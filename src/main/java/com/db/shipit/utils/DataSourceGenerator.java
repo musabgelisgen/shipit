12da8132ff0071307a89d33938d6647e245183a4
musabgelisgen <mgelisgen97@gmail.com>
2019-12-21 00:45:30 +0300

    Table generation is added

--- src/main/java/com/db/shipit/utils/DataSourceGenerator.java
+++ src/main/java/com/db/shipit/utils/DataSourceGenerator.java
@@ -40,6 +40,11 @@ public class DataSourceGenerator {
             System.exit(1);
         }
 
+        statement.executeUpdate("DROP TABLE IF EXISTS Message;");
+        statement.executeUpdate("DROP TABLE IF EXISTS Report;");
+        statement.executeUpdate("DROP TABLE IF EXISTS Subscription;");
+        statement.executeUpdate("DROP TABLE IF EXISTS Package;");
+        statement.executeUpdate("DROP TABLE IF EXISTS Route;");
         statement.executeUpdate("DROP TABLE IF EXISTS Customer;");
         statement.executeUpdate("DROP TABLE IF EXISTS CustomerService;");
         statement.executeUpdate("DROP TABLE IF EXISTS User;");
@@ -90,6 +95,87 @@ public class DataSourceGenerator {
                 "FOREIGN KEY(ID) REFERENCES User(ID)," +
                 "FOREIGN KEY(branch_name) REFERENCES Branch(name)," +
                 "CHECK(salary >= 0));");
+
+        statement.executeUpdate("CREATE TABLE Route(" +
+                "from_city VARCHAR(10)," +
+                "hub VARCHAR(10)," +
+                "to_city VARCHAR(10)," +
+                "PRIMARY KEY(from_city,to_city)," +
+                "FOREIGN KEY(from_city) REFERENCES Branch(city_name)," +
+                "FOREIGN KEY(hub) REFERENCES Branch(city_name)," +
+                "FOREIGN KEY(to_city) REFERENCES Branch(city_name));");
+
+        statement.executeUpdate("CREATE TABLE Package(" +
+                "package_id CHAR(6)," +
+                "receiver_id CHAR(6) NOT NULL," +
+                "sender_id CHAR(6) NOT NULL," +
+                "delivery_date DATETIME," +
+                "send_date DATETIME NOT NULL," +
+                "payment_side VARCHAR(8) NOT NULL," +
+                "payment_status VARCHAR(8) NOT NULL," +
+                "delivery_type VARCHAR(10) NOT NULL," +
+                "status VARCHAR(10) NOT NULL," +
+                "package_type VARCHAR(11) NOT NULL," +
+                "courier VARCHAR(20) NOT NULL," +
+                "from_city VARCHAR(10) NOT NULL," +
+                "curr_city VARCHAR(10) NOT NULL," +
+                "to_city VARCHAR(10) NOT NULL," +
+                "cost NUMERIC(10) NOT NULL," +
+                "CHECK(payment_side IN ('sender','receiver'))," +
+                "CHECK(payment_status IN ('paid','not paid'))," +
+                "CHECK(delivery_type IN ('normal','fast','super fast'))," +
+                "CHECK(status IN ('preparing', 'onTransfer', 'onBranch', 'delivered', 'declined'))," +
+                " CHECK(package_type IN ('heavy','medium','lightweight'))," +
+                "PRIMARY KEY(package_id)," +
+                "FOREIGN KEY(from_city) REFERENCES Branch(city_name)," +
+                "FOREIGN KEY(curr_city) REFERENCES Branch(city_name)," +
+                "FOREIGN KEY(to_city) REFERENCES Branch(city_name)," +
+                "FOREIGN KEY(receiver_id) REFERENCES Customer(ID)," +
+                "FOREIGN KEY(sender_id) REFERENCES Customer(ID));");
+
+        statement.executeUpdate("CREATE TABLE Subscription(" +
+                "ID CHAR(6)," +
+                "subscription_number INTEGER NOT NULL," +
+                "subscription_tier INTEGER NOT NULL," +
+                "used_package_rights INTEGER NOT NULL DEFAULT 0," +
+                "start_date DATE NOT NULL," +
+                "end_date DATE NOT NULL," +
+                "PRIMARY KEY(ID, subscription_number)," +
+                "FOREIGN KEY(ID) REFERENCES Customer(ID)," +
+                "CHECK(subscription_tier >= 0)," +
+                "CHECK((used_package_rights <= subscription_tier) AND (used_package_rights >= 0)));");
+
+        statement.executeUpdate("CREATE TABLE Report(" +
+                "report_id CHAR(6)," +
+                "handler_id CHAR(6)," +
+                "issuer_id CHAR(6) NOT NULL," +
+                "package_id CHAR(6) NOT NULL," +
+                "description VARCHAR(1000) NOT NULL," +
+                "report_type VARCHAR(7) NOT NULL," +
+                "issue_date DATETIME NOT NULL," +
+                "result VARCHAR(8)," +
+                "PRIMARY KEY(report_id)," +
+                "FOREIGN KEY(handler_id) REFERENCES CustomerService(ID)," +
+                "FOREIGN KEY(issuer_id) REFERENCES Customer(ID)," +
+                "FOREIGN KEY(package_id) REFERENCES Package(package_id)," +
+                "CHECK(report_type IN ('damaged','missing','wrong'))," +
+                "CHECK(result IN (null,'positive','negative')));");
+        
+        statement.executeUpdate("CREATE TABLE Message(" +
+                "report_id CHAR(6)," +
+                "message_number INTEGER," +
+                "text VARCHAR(1000) NOT NULL," +
+                "date DATETIME NOT NULL," +
+                "sender CHAR(6) NOT NULL," +
+                "PRIMARY KEY(report_id,message_number)," +
+                "FOREIGN KEY(report_id) REFERENCES Report(report_id)," +
+                "FOREIGN KEY(sender) REFERENCES User(ID));");
+
+
+
+
+
+
 
 
     }
